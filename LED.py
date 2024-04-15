3c82329b3a6b2d5cfd1315fda3f68a6025e9e17a
Aaron Motacek <78520107+aaron-mota@users.noreply.github.com>
2024-04-14 21:15:26 -0500

    added potentiometer file/logic

--- LED.py
+++ LED.py
@@ -1,6 +1,16 @@
 from machine import Pin
-
+from utime import sleep
 
 pinLED = Pin("LED", Pin.OUT)
-pin28 = Pin(28, Pin.OUT)
+pin15 = Pin(15, Pin.OUT)
+
+print("LED starts flashing...")
+while True:
+    try:
+        pin15.toggle()
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+pin15.off()
+print("Finished.")
 
