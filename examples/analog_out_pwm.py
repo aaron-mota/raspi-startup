52f294eeca083d6cb612b1789736d4e76faf880c
Aaron Motacek <78520107+aaron-mota@users.noreply.github.com>
2024-04-19 21:48:04 -0500

    minor changes

--- examples/analog_out_pwm.py
+++ examples/analog_out_pwm.py
@@ -1,11 +1,12 @@
 from machine import Pin, PWM
 from time import sleep
-from _pins import PIN_GP16__21_20b as PIN_GP16
+from _pins import PIN_GP15__20 as PIN_PWM
 from _convert_output_pico_potentiometer import convert_output_pico_potentiometer
-from _convert_percentage_to_voltage import convert_percentage_to_voltage
+
+# from _convert_percentage_to_voltage import convert_percentage_to_voltage
 
 # Define the PWM pin
-analogOut = PWM(Pin(PIN_GP16))
+analogOut = PWM(Pin(PIN_PWM))
 analogOut.freq(1000)  # (1000 Hz) (default is 500 Hz) (one period = 1 / frequency = 1 / 1000 = 0.001 s = 1 ms)
 analogOut.duty_u16(0)  # (0-65535) (0 = 0 V) (65535 = 3.3 V)
 
@@ -18,13 +19,16 @@ while True:
         continue
     pwmValue = convert_output_pico_potentiometer(desiredVoltage)
     analogOut.duty_u16(pwmValue)
+    sleep(0.1)
+    print("Raw: ", desiredVoltage, "Converted: ", pwmValue)
 
 # EXAMPLE TWO: Setting percentage (given numerical value between 0 and 1)
-while True:
-    desiredPercentage = float(input("Enter a percentage (0-1): "))
-    if desiredPercentage < 0 or desiredPercentage > 1:
-        print("Invalid percentage. Please enter a percentage between 0 and 1.")
-        continue
-    desiredVoltage = convert_percentage_to_voltage(desiredPercentage)
-    pwmValue = convert_output_pico_potentiometer(desiredVoltage)
-    analogOut.duty_u16(pwmValue)
+# while True:
+#     desiredPercentage = float(input("Enter a percentage (0-1): "))
+#     if desiredPercentage < 0 or desiredPercentage > 1:
+#         print("Invalid percentage. Please enter a percentage between 0 and 1.")
+#         continue
+#     desiredVoltage = convert_percentage_to_voltage(desiredPercentage)
+#     pwmValue = convert_output_pico_potentiometer(desiredVoltage)
+#     analogOut.duty_u16(pwmValue)
+#     sleep(0.1)
