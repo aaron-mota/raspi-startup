1420facfa41784260e059a1ff004fe9115c42f10
Aaron Motacek <78520107+aaron-mota@users.noreply.github.com>
2024-04-28 14:58:33 -0500

    finished serv

--- components/servo_sg90.py
+++ components/servo_sg90.py
@@ -3,6 +3,7 @@ from time import sleep
 
 from _pins import (
     PIN_GP15__20 as PIN_GP15,
+    PIN_GP26__31_10b as PIN_GP26,
 )
 
 from _constants import PICO_MIN_PWM_ACTUAL, PICO_MAX_PWM
@@ -30,8 +31,14 @@ from _constants import PICO_MIN_PWM_ACTUAL, PICO_MAX_PWM
 #################
 # HELPERS
 #################
+PERIOD = 20
+
+MIN_PULSE_WIDTH = 0.5
+MAX_PULSE_WIDTH = 2.5
+MAX_DEGREES = 180
+
+
 def convertPulseWidthToPWMValue(pulseWidth: float) -> int:
-    PERIOD = 20
     return int(round(PICO_MAX_PWM * (pulseWidth / PERIOD)))
 
 
@@ -40,9 +47,6 @@ def convertDegreesToPulseWidth(degrees: int) -> float:
     # 0 degrees: 0.5ms pulse
     # 90 degrees: 1.5ms pulse
     # 180 degrees: 2.5ms pulse
-    MIN_PULSE_WIDTH = 0.5
-    MAX_PULSE_WIDTH = 2.5
-    MAX_DEGREES = 180
     return MIN_PULSE_WIDTH + ((MAX_PULSE_WIDTH - MIN_PULSE_WIDTH) / MAX_DEGREES) * degrees
 
 
@@ -51,7 +55,9 @@ def convertDegreesToPulseWidth(degrees: int) -> float:
 #################
 # Step 1: Define the pins
 servo = PWM(Pin(PIN_GP15))
+servo.freq(50)  # 50Hz
 
+potentiometer = ADC(Pin(PIN_GP26))
 
 #################
 # PROGRAM
@@ -60,14 +66,28 @@ servo = PWM(Pin(PIN_GP15))
 degrees = 0
 try:
     while True:
+        # (1) GET DEGREES
+        #################
+        # HARD-CODED
         # degrees = 180
-        result = int(input("Enter degrees (0 to 180): "))
-        degrees = result
+
+        # HARD-CODED (AUTO TOGGLE)
+        # degrees = 90 if degrees == 0 else 180 if degrees == 90 else 0 # auto toggle between 0, 90, 180
+
+        # USER INPUT
+        # result = int(input("Enter degrees (0 to 180): "))
+        # degrees = result
+
+        # POTENTIOMETER
+        pwmValueInput = potentiometer.read_u16()
+        degrees = int(round((MAX_DEGREES / PICO_MAX_PWM) * pwmValueInput))  # not sure if this is correct (didn't test it)
+
+        # (2) SET SERVO
+        #################
         pulseWidth = convertDegreesToPulseWidth(degrees)
         pwmValue = convertPulseWidthToPWMValue(pulseWidth)
-        print(f"degrees: {degrees}, pulseWidth: {pulseWidth}, pwmValue: {pwmValue}")
         servo.duty_u16(pwmValue)
-        sleep(2)
-        # degrees = 90 if degrees == 0 else 180 if degrees == 90 else 0 # auto toggle between 0, 90, 180
+        sleep(0.02)
+        # print(f"degrees: {degrees}, pulseWidth: {pulseWidth}, pwmValue: {pwmValue}")
 except KeyboardInterrupt:
     servo.duty_u16(0)
