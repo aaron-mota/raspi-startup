f1dad938823cfc55b40b7c57d7d36df638e4f9f0
Aaron Motacek <78520107+aaron-mota@users.noreply.github.com>
2024-04-28 15:58:11 -0500

    setup py-mc template (pythonMicrocontroller)

---
+++ components/gyroscope_accelerometer_MPU6050.py
@@ -0,0 +1,67 @@
+from machine import Pin, PWM, ADC
+from time import sleep
+
+# from _pins import (
+# PIN_GP15__20 as PIN_GP15,
+# PIN_GP26__31_10b as PIN_GP26,
+# )
+
+# from _constants import PICO_MIN_PWM_ACTUAL, PICO_MAX_PWM
+
+# from _convert_output_voltage_to_pwm import convert_output_voltage_to_pwm
+# from _convert_percentage_to_voltage import convert_percentage_to_voltage
+# from _convert_output_pwm_to_voltage import convert_output_pwm_to_voltage
+# from _convert_minMax_actual_to_desired import convert_minMax_actual_to_desired
+
+#################
+# NOTES
+#################
+# MPU6050
+# - https://docs.sunfounder.com/projects/ultimate-sensor-kit/en/latest/components_basic/05-component_mpu6050.html
+# - components: 3-axis gyroscope, 3-axis accelerometer
+
+
+#################
+# HELPERS
+#################
+
+
+#################
+# SETUP
+#################
+
+
+#################
+# PROGRAM
+#################
+
+try:
+    while True:
+        #################
+        # (1) GET INPUT
+        #################
+        # HARD-CODED
+        # ...
+
+        # HARD-CODED (AUTO TOGGLE)
+        # ...
+
+        # USER INPUT
+        # ...
+
+        # POTENTIOMETER
+        # ...
+
+        #################
+        # (2) SET OUTPUT
+        #################
+
+        sleep(0.1)
+        # print("")
+except KeyboardInterrupt:
+    print("Bye!")
+
+
+# MPU6050
+# https://docs.sunfounder.com/projects/ultimate-sensor-kit/en/latest/components_basic/05-component_mpu6050.html
+# - components: 3-axis gyroscope, 3-axis accelerometer
